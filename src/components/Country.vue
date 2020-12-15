dfc70fb8a856461c4498f67cc5b971a469ca6b66
Alicia Sykes <sykes.alicia@gmail.com>
2020-12-15 16:53:20 +0000

    Adds resonsive layout for mobile, simple nav bar, and installs console new font

--- src/components/Country.vue
+++ src/components/Country.vue
@@ -30,40 +30,16 @@ export default {
 <style lang="scss">
 
 div.country { // Country container
-
   display: flex;
   flex-direction: row-reverse;
   justify-content: flex-end;
   align-items: center;
-	margin: 0.5rem;
+  margin: 0.5rem auto;
+  max-width: 650px;
 	background: var(--foreground);
 	border-radius: 5px;
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
   transition: all 0.3s cubic-bezier(.25,.8,.25,1);
-
-
-  div.country-main-info-wrapper {
-
-      display: flex;
-      flex-direction: initial;
-      align-items: baseline;
-
-      h4.country-name {
-        margin: 0;
-        display: inline;
-        font-size: 1rem;
-      }
-
-      p.country-capital {
-        font-size: 1rem;
-      }
-
-      span.line-contector {
-        margin: 0 0.5rem;
-      }
-  }
-
-
 
   div.country-flag {
     img {
@@ -72,6 +48,39 @@ div.country { // Country container
     }
   }
 
+  div.country-main-info-wrapper {
+
+      display: flex;
+      flex-direction: initial;
+      align-items: baseline;
+      margin: 0 0.5rem;
+
+      h4.country-name {
+        font-size: 1rem;
+        margin: 0;
+      }
+
+      p.country-capital {
+        font-size: 1rem;
+        margin: 0;
+        color: var(--small-text);
+      }
+
+      span.line-contector {
+        margin: 0 0.5rem;
+      }
+
+      // Adjustments for text on mobile country list
+      @media (max-width: 600px) {
+        flex-direction: column;
+        span.line-contector {
+          display: none;
+        }
+        h4.country-name {
+          margin-bottom: 0.1rem;
+        }
+      }
+  }
 }
 
 </style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="layout">
     <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
+      <h1>
+        <g-link to="/">🌍 {{ $static.metadata.siteName }}</g-link>
+      </h1>
     </header>
+    <article class="main-content">
+
+    </article>
     <slot/>
   </div>
 </template>
@@ -21,30 +20,35 @@ query {
 }
 </static-query>
 
-<style>
+<style lang="scss">
+
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
   margin:0;
   padding:0;
-
 }
 
-.layout {
+.main-content {
   max-width: 760px;
   margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
+  padding: 0 1rem;
 }
 
 .header {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
+  margin-bottom: 1rem;
+  padding: 1rem;
+  background: var(--foreground);
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 75%);
+  h1 a {
+    color: var(--text-color);
+    text-decoration: none;
+    font-size: 2rem;
+    font-variation-settings: 'wght' 200;
+  }
 }
 
-.nav__link {
-  margin-left: 20px;
-}
+
+
 </style>
